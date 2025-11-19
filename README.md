99655376b47de6141e7a87802bac1c253f3e4926
hamsang <hamsang@exciform.com>
2025-11-19 16:43:52 +0900

    ✨ feat(programmers): 프로그래머스 120812 최빈값 찾기 풀이
    - 문제 블로그 링크 README.md에 첨부

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ https://programmers.co.kr/
 ## 120812 최빈값 구하기
 - [문제 원본](https://school.programmers.co.kr/learn/courses/30/lessons/120812)
 - [문제 풀이](programmers/120812_find_mode.py)
-- [문제 블로그]()
+- [문제 블로그](https://sailorham.tistory.com/2)
--- programmers/120812_find_mode.py
+++ programmers/120812_find_mode.py
@@ -1,6 +1,23 @@
 def solution(array):
 
-    return 0
+    occurred_dictionary = {}
+
+    for number in array:
+        occurred_dictionary[number] = occurred_dictionary.get(number, 0) + 1
+
+    mode_number = 0
+    mode_count = 0
+
+    for number, count in occurred_dictionary.items():
+        if count > mode_count:
+            mode_number = number
+            mode_count = count
+
+    for number, count in occurred_dictionary.items():
+        if number != mode_number and count == mode_count:
+            return -1
+
+    return mode_number
 
 print(solution([1, 2, 3, 3, 3, 4]))
 print(solution([1, 1, 2, 2]))
