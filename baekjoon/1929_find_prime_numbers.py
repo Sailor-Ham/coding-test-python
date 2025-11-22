8cf4ed5a0f1485de6e2e520252895460f7674389
Sailor-Ham <sangja234@gmail.com>
2025-11-22 09:55:50 +0900

    ✨ feat(baekjoon): 백준 1929 소수 구하기 풀이

--- baekjoon/1929_find_prime_numbers.py
+++ baekjoon/1929_find_prime_numbers.py
@@ -1,1 +1,17 @@
-# 1929 - 소수 구하기
+import sys
+import math
+
+read = sys.stdin.readline
+sqrt = math.sqrt
+
+M, N = map(int, read().split())
+
+for n in range(M, N + 1):
+    if n == 1:
+        continue
+
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            break
+    else:
+        print(n)
