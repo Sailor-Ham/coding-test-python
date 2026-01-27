292c9c6691cbc339c3561e9fff2f86b9ce512717
sailorham <sangja234@gmail.com>
2026-01-27 18:49:37 +0900

    📝 docs: README에 프로그래머스 120812 최빈값 구하기 문제 추가 및 최빈값 구하기 문제 코드 스니펫 작성

---
+++ programmers/120812_find_mode.py
@@ -0,0 +1,12 @@
+def solution(array):
+    answer = 0
+    return answer
+
+
+array1 = [1, 2, 3, 3, 3, 4]
+array2 = [1, 1, 2, 2]
+array3 = [1]
+
+print(solution(array1))
+print(solution(array2))
+print(solution(array3))
