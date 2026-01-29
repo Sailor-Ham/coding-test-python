a5014544b33062513a6f32fd3c8517bc801f6823
Sailor-Ham <sangja234@gmail.com>
2026-01-29 18:39:34 +0900

    ✨ feat(baekjoon): 백준 1439 뒤집기 문제 풀이
    - 블로그 작성
    - README에 동기화

--- baekjoon/1439_reverse.py
+++ baekjoon/1439_reverse.py
@@ -1,1 +1,27 @@
 # 백준 1439 - 뒤집기
+
+import sys
+
+input = sys.stdin.readline
+
+string = input().rstrip()
+
+count_to_all_zero = 0
+count_to_all_one = 0
+
+if string[0] == "1":
+    count_to_all_zero += 1
+else:
+    count_to_all_one += 1
+
+for idx in range(len(string) - 1):
+    curr = string[idx]
+    next = string[idx + 1]
+
+    if curr != next:
+        if next == "1":
+            count_to_all_zero += 1
+        else:
+            count_to_all_one += 1
+
+print(min(count_to_all_zero, count_to_all_one))
