237dc654359e066769e939a8d6127674622d0090
Sailor-Ham <sangja234@gmail.com>
2026-02-07 15:29:57 +0900

    ✨ feat(baekjoon): 백준 1874 스택 수열 문제 풀이
    - 블로그 작성
    - README에 동기화

--- baekjoon/1874_stack_sequence.py
+++ baekjoon/1874_stack_sequence.py
@@ -1,1 +1,34 @@
 # 백준 1874 - 스택 수열
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+targets = []
+for _ in range(N):
+    targets.append(int(input()))
+
+stack = []
+result = []
+
+cur = 1
+is_possible = True
+
+for target in targets:
+    while cur <= target:
+        stack.append(cur)
+        result.append("+")
+        cur += 1
+
+    if stack and stack[-1] == target:
+        stack.pop()
+        result.append("-")
+    else:
+        is_possible = False
+        break
+
+if is_possible:
+    print("\n".join(result))
+else:
+    print("NO")
