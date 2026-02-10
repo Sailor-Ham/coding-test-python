8407b5b2d03d914f87a4331a1fc31c00c7306370
Sailor-Ham <sangja234@gmail.com>
2026-02-10 11:47:50 +0900

    ✨ feat(programmers): 프로그래머스 42584 주식가격 문제 풀이
    - 블로그 작성
    - README에 동기화

--- programmers/42584_stock_price.py
+++ programmers/42584_stock_price.py
@@ -1,5 +1,27 @@
 # 프로그래머스 42584 - 주식가격
 
+from collections import deque
+
+
 def solution(prices):
     answer = []
+    prices_queue = deque(prices)
+
+    while prices_queue:
+        cur_price = prices_queue.popleft()
+        period = 0
+
+        for next_price in prices_queue:
+            if cur_price > next_price:
+                break
+
+            period += 1
+
+        answer.append(period)
+
     return answer
+
+
+prices = [1, 2, 3, 2, 3]
+
+print(solution(prices))
